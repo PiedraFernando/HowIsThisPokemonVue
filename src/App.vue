5d4fff4bd77de644e8c338fd555832f59deb5f44
Fernando Piedra <fernandopiedram@gmail.com>
2021-11-02 01:32:33 -0600

    first commid, functionality

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,127 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-center mt-3">How Is this Pokemon</h1>
+  <p class="text-center">Game about, how is this pokemon. By: Fernando Piedra Mendez &lt;OnlyKal&gt;</p>
+  <div class="d-flex justify-content-center mt-3">
+    <h3 class="m-3">Corrects: {{corrects}}</h3>
+    <h3 class="m-3">Errors: {{errors}}</h3>
+  </div>
+  <div class="d-flex justify-content-center mt-5">
+    <Pokemon :nombre="nameAnsware" :img="img" :status="status" />
+  </div>
+  <h2 class="text-center mt-3">How is?</h2>
+  <div class="d-flex justify-content-center mt-5">
+    <button type="button" class="btn btn-outline-secondary m-2" @click="check1">{{name1}}</button>
+    <button type="button" class="btn btn-outline-secondary m-2" @click="check2">{{name2}}</button>
+    <button type="button" class="btn btn-outline-secondary m-2" @click="check3">{{name3}}</button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Pokemon from "./components/Pokemon.vue";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Pokemon
+  },
+  data(){
+    return{
+      nameAnsware: null,
+      errors:0,
+      corrects:0,
+      status: false,
+      name1: null,
+      name2: null,
+      name3: null,
+      img: null
+    }
+  },
+  created(){
+    this.getAskPokemon()
+  },
+  methods:{
+    getAskPokemon(){
+      fetch('https://pokeapi.co/api/v2/pokemon/' + (Math.floor(Math.random() * 151) + 1))
+        .then(response => response.json())
+        .then(data => {
+          let num = Math.floor(Math.random() * 3) + 1
+          this.img = data.sprites.front_default
+          this.nameAnsware = data.name
+          if(num === 1){
+            this.name1 = data.name
+          }
+          if(num === 2){
+            this.name2 = data.name
+          }
+          if(num === 3){
+            this.name3 = data.name
+          }
+          if(!this.name1){
+            fetch('https://pokeapi.co/api/v2/pokemon/' + (Math.floor(Math.random() * 151) + 1))
+            .then(response => response.json())
+            .then(data => {
+              this.name1 = data.name
+            })
+          }
+          if(!this.name2){
+            fetch('https://pokeapi.co/api/v2/pokemon/' + (Math.floor(Math.random() * 151) + 1))
+            .then(response => response.json())
+            .then(data => {
+              this.name2 = data.name
+            })
+          }
+          if(!this.name3){
+            fetch('https://pokeapi.co/api/v2/pokemon/' + (Math.floor(Math.random() * 151) + 1))
+            .then(response => response.json())
+            .then(data => {
+              this.name3 = data.name
+            })
+          }
+        })
+    },
+    check1(){
+      if(this.nameAnsware === this.name1){
+        this.corrects ++;
+      }else{
+        this.errors ++;
+      }
+      this.restartName()
+    },
+    check2(){
+      if(this.nameAnsware === this.name2){
+        this.corrects ++;
+      }else{
+        this.errors ++;
+      }
+      this.restartName()
+    },
+    check3(){
+      if(this.nameAnsware === this.name3){
+        this.corrects ++;
+      }else{
+        this.errors ++;
+      }
+      this.restartName()
+    },
+    restartName(){
+      this.status = true
+      setTimeout(() => {
+        this.name1 = null
+        this.name2 = null
+        this.name3 = null
+        this.status = false
+        this.getAskPokemon()
+      }, 1000);
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+    font-family: 'Gochi Hand', cursive;
+    font-family: 'Zen Kurenaido', sans-serif;
+    background-color: rgb(63, 63, 63);
+    color: rgb(221, 221, 221);
 }
 </style>
---
+++ src/components/Pokemon.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+        <template v-if="status == true">
+            <img :src="img" :alt="nombre">
+            <p class="text-center">{{nombre}}</p>
+        </template>
+        <template v-else>
+            <img class="gray" :src="img" :alt="nombre">
+            <p class="text-center">????????</p>
+        </template>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Pokemon',
+    props: ['nombre', 'img', 'status']
+}
+</script>
+
+<style scoped>
+    *{
+        border-radius: 10px;
+        font-size: 25px;
+        background: rgb(112, 112, 112);
+    }
+    img{
+        width: 220px;
+    }
+    .gray{
+        filter: grayscale(100%);
+    }
+</style>
