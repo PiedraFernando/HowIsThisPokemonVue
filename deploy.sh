55ed93d6658ba9e04de249509739f183e810b5c3
Fernando Piedra <fernandopiedram@gmail.com>
2021-11-02 11:33:42 -0600

    config fix

--- deploy.sh
+++ deploy.sh
@@ -10,6 +10,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:PiedraFernando/HowIsThisPokemonVue.git master:gh-pages
+git push -f git@github.com:PiedraFernando/WhoIsThisPokemonVue.git master:gh-pages
 
 cd -
